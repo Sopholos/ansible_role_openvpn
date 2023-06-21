d4990df10c8dffcd2054f149a142846f00dcc7ee
Tomilov Alexander <sopholos@sopholos.ru>
2023-06-21 15:58:34 +0500

    logs improvements extras from env

--- docker_client/entrypoint.sh
+++ docker_client/entrypoint.sh
@@ -15,7 +15,7 @@ VPN_OVPN="$VPN_SERVICE_FOLDER/$VPN_SERVER.ovpn"
 VPN_AUTH_FILE="/services/login.key"
 
 # Extra params passed to openvpn client. See https://community.openvpn.net/openvpn/wiki/Openvpn23ManPage
-VPN_EXTRAS="--ping 15"
+# VPN_EXTRAS="${VPN_EXTRAS:---ping 15}"
 
 # The following are from the .env file
 # VPN_SERVICE
@@ -27,6 +27,8 @@ VPN_EXTRAS="--ping 15"
 # END CONFIG
 ###################
 
+TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S.%N"`
+
 # Setup the user/pass login file
 rm -f $VPN_AUTH_FILE
 touch $VPN_AUTH_FILE
@@ -36,22 +38,22 @@ echo $VPN_PASS >> $VPN_AUTH_FILE
 
 # Debugging
 echo "
--- VPN Configuration ---
+$TIMESTAMP -- VPN Configuration ---
 "
 
-echo "oVPN configuration file: $VPN_OVPN"
+echo "$TIMESTAMP oVPN configuration file: $VPN_OVPN"
 
-echo "VPN auth file: $VPN_AUTH_FILE"
+echo "$TIMESTAMP VPN auth file: $VPN_AUTH_FILE"
 
 # Verify the vpn service file exists
 if [ ! -d "$VPN_SERVICE_FOLDER" ]; then
-	echo "Invalid VPN service: $VPN_SERVICE"
+	echo "$TIMESTAMP Invalid VPN service: $VPN_SERVICE"
 	exit
 fi
 
 # Verify the vpn config file exists
 if [ ! -f "$VPN_OVPN" ]; then
-	echo "Unable to find VPN server: $VPN_SERVER"
+	echo "$TIMESTAMP Unable to find VPN server: $VPN_SERVER"
 	exit
 fi
 
@@ -60,7 +62,7 @@ echo 'nameserver 1.1.1.1' > /etc/resolv.conf
 
 # Kick off the connection
 echo "
---- Starting VPN Client ---
+$TIMESTAMP --- Starting VPN Client ---
 "
 
 # See https://community.openvpn.net/openvpn/wiki/Openvpn23ManPage
